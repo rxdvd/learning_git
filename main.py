2406efa8585feb30d021a29f845d1c1149c160b6
Jimmy Cheong <jimmycheong7@gmail.com>
2018-12-26 13:07:54 +0000

    Time is money

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ This is just a test file
 def main():
     print("Hello world!")
 
+    time = "important"
+    money = "important"
+
+    if time == money: 
+        print("Time is money")
+
 if __name__ == '__main__':
     main()
